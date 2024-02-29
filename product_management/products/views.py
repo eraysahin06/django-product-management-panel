9edfebcd5349e99f50367b002b8c6d40e40e9911
Eray Sahin <eraysahinmoon@gmail.com>
2024-02-28 20:27:05 -0500

    added product views

--- product_management/products/views.py
+++ product_management/products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products': products})
+
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, 'products/product_detail.html', {'product': product})
